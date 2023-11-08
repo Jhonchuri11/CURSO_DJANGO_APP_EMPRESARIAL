19d12822b65ed2de8809ea474b32812add14ca23
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-11-08 09:03:38 -0500

    La URL editar, deshabilitando, ya que no permite editar y con esto la aplicacion funciona solo con agregar y eliminar un alumno

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from . import views
+from web import views
 
 app_name = 'web'
 
 urlpatterns = [
      path('', views.AlumnoView.as_view(), name='index'),
      path('eliminarAlumno/<alumno_id>/', views.AlumnoView.deleteAlum, name='eliminarAlumno'),
-     path('editarAlumno/<int:alumno_id>/', views.AlumnoView.editarAlumno, name='editarAlumno'),
+     
      path('profesor', views.ProfesorView.as_view(), name='profesor'),
      path('eliminarProfesor/<profesor_id>/', views.ProfesorView.deleteProfesor, name='eliminarProfesor'),
      
