68687249192ccd80741bdbd38e07804929b85e56
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-11-08 08:45:49 -0500

    Subiendo proyecto de Desarrollo Empresarial, para el controlamiento de alumnos y formularios de registro

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = 'web'
 
 urlpatterns = [
-    path('', views.index,name='index')
+     path('', views.AlumnoView.as_view(), name='index'),
+     path('eliminarAlumno/<alumno_id>/', views.AlumnoView.deleteAlum, name='eliminarAlumno'),
+     path('editarAlumno/<int:alumno_id>/', views.AlumnoView.editarAlumno, name='editarAlumno'),
+     path('profesor', views.ProfesorView.as_view(), name='profesor'),
+     path('eliminarProfesor/<profesor_id>/', views.ProfesorView.deleteProfesor, name='eliminarProfesor'),
+     
+     
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,60 @@
-from django.shortcuts import render
-# importamos la libreria generic
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+# importamos la clase View
+from django.views import View
+from .models import *
+from .forms import *
+from django.contrib import messages
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+class AlumnoView(View):
+    
+    def get(self,request):
+        listaAlumnos = TblAlumno.objects.all()
+        formAlumno = AlumnoForm()
+        context = {
+            'alumnos' : listaAlumnos,
+            'formAlumno': formAlumno
+        }
+        return render(request,'index.html',context)
+
+    def post(self, request):
+        formAlumno = AlumnoForm(request.POST)
+        if formAlumno.is_valid():
+            formAlumno.save()
+            return redirect('/')
+        
+    def deleteAlum(request, alumno_id):
+        Alumno = get_object_or_404(TblAlumno, alumno_id=alumno_id)
+        Alumno.delete()
+        return redirect('/')
+    
+    def editarAlumno(request, alumno_id):
+        alumno = get_object_or_404(TblAlumno, alumno_id=alumno_id)
+        data = {
+            'form' : AlumnoForm(instance=alumno)
+        }
+        return render(request, 'editar_alumno.html', data)
+        
+    
+class ProfesorView(View):
+
+    def get(self,request):
+        listProfesores = TblProfesor.objects.all()
+        formProfesor = ProfesorForm()
+        context = {
+            'profesores': listProfesores,
+            'formProfesor': formProfesor
+        }
+        return render(request, 'profesores.html', context)
+    
+    def post(self, request):
+        formProfesor = ProfesorForm(request.POST)
+        if formProfesor.is_valid():
+            formProfesor.save()
+            return redirect('/profesor')
+    def deleteProfesor(request, profesor_id):
+        profesor = get_object_or_404(TblProfesor, profesor_id=profesor_id)
+        profesor.delete()
+        return redirect('/profesor')
